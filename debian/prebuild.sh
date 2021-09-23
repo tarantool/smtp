decdcf3b71db880b2022bb546c8a5476510a378c
Roman Khabibov <roman.habibov@tarantool.org>
2021-09-23 23:50:40 +0300

    build: enable tarantool repo for rpmbuild/debuild
    There are distributions, where tarantool is in base repositories (such
    as Fedora 30-34 or Ubuntu Focal). There are ones, where it is in
    semi-official repos such as EPEL (CentOS 7/8). There are ones, where we
    have no tarantool package at all (or have only old tarantool-lts as in
    Ubuntu Bionic).
    
    It seems meaningful to enable the tarantool repository everywhere and
    don't depend on presence of a tarantool package in distribution base
    repositories.
    
    The repository is enabled using the installer.sh script: see [1] for
    details. Aside of just enabling the repo it configures a package manager
    (apt-get/yum/dnf) to prefer a package from our repository even when it
    has lower version. So we'll build against latest 1.10 on all
    distributions, it looks as the good side effect: unification.
    
    Worked around a problem with pinned tarantool package and unpinned
    tarantool-dev, see [2].
    
    [1]: https://github.com/tarantool/installer.sh
    [2]: https://github.com/tarantool/installer.sh/issues/10
    
    Part of #34
    
    Co-authored-by: Alexander Turenko <alexander.turenko@tarantool.org>

---
+++ debian/prebuild.sh
@@ -0,0 +1,6 @@
+curl -LsSf https://www.tarantool.io/release/1.10/installer.sh | sudo bash
+
+# Workaround https://github.com/tarantool/installer.sh/issues/10
+printf '%s\n' 'Package: tarantool-dev'             | sudo tee    /etc/apt/preferences.d/tarantool-dev
+printf '%s\n' 'Pin: origin download.tarantool.org' | sudo tee -a /etc/apt/preferences.d/tarantool-dev
+printf '%s\n' 'Pin-Priority: 1001'                 | sudo tee -a /etc/apt/preferences.d/tarantool-dev
